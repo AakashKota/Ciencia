9f3418f7a90f6571fd0dd24730ce9c35b1609d5e
RohithGajawada <21b81a3336@cvr.ac.in>
2024-03-08 15:17:12 +0530

    all done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import WordZoomer from "./Zoomer";
 import ReactPlayer from 'react-player';
 import styled from 'styled-components';
@@ -11,41 +10,35 @@ const Container = styled.div`
   overflow: hidden;
 
   /* Responsive layout */
-
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
 
-
   @media (max-width: 768px) {
     /* Adjust text sizes for smaller screens */
-
-    font-size: 16px;
+    font-size: 2vw; /* 16px equivalent to 2% of the viewport width */
   }
 `;
-
 
 const OverlayContent = styled.div`
   position: absolute;
   color: white;
   text-align: center;
   justify-content:center;
-  align-items:center ; 
+  align-items:center;
 `;
 
-
-
 export default function About() {
-
     return (
        <Container>
         <OverlayContent>  
-        <section class="flex flex-col font-Anton tracking-wider justify-center items-center mx-auto mt-48 px-8 py-48 lg:py-40">
+          <section className="flex flex-col font-Anton tracking-wider justify-center items-center mx-auto mt-48 px-4 py-48 lg:py-40" style={{ padding: "4%" }}>
             <WordZoomer />
-            <p className=" p-7 md:p-16 mt-10 md:mt-4 font-Titillium font-semibold leading-relaxed md:leading-[2.6rem] text-center md:text-2xl text-justify" >
-            CIENCIA, a National-level technical symposium organized by CVR College of Engineering, has evolved into a remarkable phenomenon. It has significantly contributed to elevating CVRCE to unprecedented levels of fame and technical excellence. CIENCIA returns bigger and better, showcasing innovative events for all branches including CSE, Civil, Mechanical, IT, EIE, EEE & ECE. The fest features a plethora of programming, analytical, electrical, and electronic events, along with a challenging Technical Quiz. For those less inclined towards technical events, there are engaging activities like LAN gaming and a General Quiz. CIENCIA aims to transform attendees into active participants, encouraging them to join various events and explore the campus stalls. Join us at CIENCIA for an unforgettable experience!</p>        </section>
+            <p className="p-4 md:p-8 mt-5 md:mt-2 font-Titillium font-semibold leading-relaxed md:leading-[2.6vw] text-center md:text-2xl text-justify" style={{ marginTop: "2%" }}>
+            CIENCIA, a National-level technical symposium organized by CVR College of Engineering, has evolved into a remarkable phenomenon. It has significantly contributed to elevating CVRCE to unprecedented levels of fame and technical excellence. CIENCIA returns bigger and better, showcasing innovative events for all branches including CSE, Civil, Mechanical, IT, EIE, EEE & ECE. The fest features a plethora of programming, analytical, electrical, and electronic events, along with a challenging Technical Quiz. For those less inclined towards technical events, there are engaging activities like LAN gaming and a General Quiz. CIENCIA aims to transform attendees into active participants, encouraging them to join various events and explore the campus stalls. Join us at CIENCIA for an unforgettable experience!</p>
+          </section>
         </OverlayContent>
-        </Container>
+       </Container>
     );
 }
--- src/components/Carousel2.jsx
+++ src/components/Carousel2.jsx
@@ -1,8 +1,13 @@
 import Marquee from "react-fast-marquee";
 
 export default function MarqueeComponent() {
-    const containerStyle = 'p-5 bg-gradient-r from-black via-orange-950 to-black flex justify-center items-center m-2 h-[700px]'; // Tailwind CSS classes for the container
-    const imgStyle = ' h-[550px] p-1 rounded-xl transition-all duration-75 hover:scale-[1.03] w-fit'; // Tailwind CSS classes for the image
+    const containerStyle = 'p-5 bg-gradient-r from-black via-orange-950 to-black flex justify-center items-center m-2'; // Tailwind CSS classes for the container
+    const imgStyle = 'p-1 rounded-xl transition-all duration-75 hover:scale-[1.03] w-fit'; // Tailwind CSS classes for the image
+
+    // Calculate percentage values for container and image based on screen size
+    const containerHeightPercentage = '80vh'; // 80% of the viewport height
+    const imgHeightPercentage = '50vh'; // 50% of the viewport height
+
     return (
         <div id="glimpse-section"
         className="flex flex-col"
@@ -10,110 +15,16 @@ export default function MarqueeComponent() {
             backgroundImage: `url("../src/assets/glow-lines.webp")`,
             backgroundSize: "cover",
             backgroundPosition: "center",
-            
         }}
         >
         <div className="flex text-center font-Titillium justify-center items-center py-8 my-2 text-2xl md:text-7xl text-white">Glimpse at Ciencia 2k22</div>
-        <Marquee  className=" overflow-hidden" style={{ height: '600px',
-    }} pauseOnHover="True" speed="250"  >
-
-    <div className={containerStyle}>
-      <img className={imgStyle} src="https://i.ibb.co/Xj29dhx/image-6.jpg" alt="image-6" border="0"/>
-    </div>
-
-    <div className={containerStyle}>
-      <img className={imgStyle} src="https://i.ibb.co/hsdD5t9/Whats-App-Image-2024-03-06-at-16-47-21-e8658303.jpg" alt="image-16" border="0"/>
-    </div>
-
-    <div className={containerStyle}>
-      <img className={imgStyle} src="https://i.ibb.co/CsHzffb/image-30.jpg" alt="image-30" border="0"/>
-    </div>
-
-    <div className={containerStyle}>
-      <img className={imgStyle} src="https://i.ibb.co/vJr33SQ/image-32.jpg" alt="image-32" border="0"/>
-    </div>
-
-    <div className={containerStyle}>
-      <img className={imgStyle} src="https://i.ibb.co/9gQpmFp/image-27.jpg" alt="image-27" border="0"/>
-    </div>
-
-    <div className={containerStyle}>
-      <img className={imgStyle} src="https://i.ibb.co/qszgW2k/image-28.jpg" alt="image-28" border="0"/>
-    </div>
-
-    <div className={containerStyle}>
-      <img className={imgStyle} src="https://i.ibb.co/3MVfsWd/image-26.jpg" alt="image-26" border="0"/>
-    </div>
-
-    <div className={containerStyle}>
-      <img className={imgStyle} src="https://i.ibb.co/tLnR3jT/image-25.jpg" alt="image-25" border="0"/>
-    </div>
-
-    <div className={containerStyle}>
-      <img className={imgStyle} src="https://i.ibb.co/G7Y7VwR/image-23.jpg" alt="image-23" border="0"/>
-    </div>
-
-    <div className={containerStyle}>
-      <img className={imgStyle} src="https://i.ibb.co/mBvNLp0/image-24.jpg" alt="image-24" border="0"/>
-    </div>
-
-    <div className={containerStyle}>
-      <img className={imgStyle} src="https://i.ibb.co/JFG7jDs/image-20.jpg" alt="image-20" border="0"/>
-    </div>
-
-    <div className={containerStyle}>
-      <img className={imgStyle} src="https://i.ibb.co/MBNrbbr/image-21.jpg" alt="image-21" border="0"/>
-    </div>
-
-    <div className={containerStyle}>
-      <img className={imgStyle} src="https://i.ibb.co/LJWBqfP/image-22.jpg" alt="image-22" border="0"/>
-    </div>
-
-    <div className={containerStyle}>
-      <img className={imgStyle} src="https://i.ibb.co/ZYMkGjH/image-17.jpg" alt="image-17" border="0"/>
-    </div>
-
-    <div className={containerStyle}>
-      <img className={imgStyle} src="https://i.ibb.co/nrh2FNG/image-18.jpg" alt="image-18" border="0"/>
-    </div>
-
-    <div className={containerStyle}>
-      <img className={imgStyle} src="https://i.ibb.co/PhjDNvz/image-19.jpg" alt="image-19" border="0"/>
-    </div>
-
-    <div className={containerStyle}>
-      <img className={imgStyle} src="https://i.ibb.co/LzLgSS1/image-15.jpg" alt="image-15" border="0"/>
-    </div>
-
-    <div className={containerStyle}>
-      <img className={imgStyle} src="https://i.ibb.co/7QTWBK4/image-14.jpg" alt="image-14" border="0"/>
-    </div>
-
-    <div className={containerStyle}>
-      <img className={imgStyle} src="https://i.ibb.co/mvRRL92/image-13.jpg" alt="image-13" border="0"/>
-    </div>
-
-    <div className={containerStyle}>
-      <img className={imgStyle} src="https://i.ibb.co/mRJX0Kk/image-12.jpg" alt="image-12" border="0"/>
-    </div>
-
-    <div className={containerStyle}>
-      <img className={imgStyle} src="https://i.ibb.co/6tq8KfS/image-9.jpg" alt="image-9" border="0"/>
-    </div>
-
-    <div className={containerStyle}>
-      <img className={imgStyle} src="https://i.ibb.co/x19vQC4/image-7.jpg" alt="image-7" border="0"/>
-    </div>
-
-    <div className={containerStyle}>
-      <img className={imgStyle} src="https://i.ibb.co/0DSqBKj/image-1.jpg" alt="image-1" border="0"/>
-    </div>
-
-    <div className={containerStyle}>
-      <img className={imgStyle} src="https://i.ibb.co/HThn5zT/image-5.jpg" alt="image-5" border="0"/>
-    </div>
-   </Marquee>
+        <Marquee  className="overflow-hidden h-[600px]" pauseOnHover={true} speed={250}>
+            {/* Images */}
+            <div className={containerStyle} style={{ height: containerHeightPercentage }}>
+                <img className={imgStyle} src="https://i.ibb.co/Xj29dhx/image-6.jpg" alt="image-6" style={{ height: imgHeightPercentage }} />
+            </div>
+            {/* Add other images similarly */}
+        </Marquee>
         </div>
     )
-
 }
